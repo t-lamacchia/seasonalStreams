e50bfc473dd7f1a6253aab66e663ef30ae2ee331
t-lamacchia <lamacchiat2007@gmail.com>
2024-11-14 12:40:17 -0500

    java

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 console.log('Hello!');
+const todaysDate = () => {
+    let d = new Date();
+    return "Today's date is " + d
+}
+
+const dateblock = document.getElementById("dateblock")
+dateblock.innerText = todaysDate();
+
+
+const body = document.body
+const d = new Date();
+
+if(d.getHours()> 20 || d.getHours()< 8){
+    body.style["backgroundColor"] = "black";
+    body.style["color"] = "white";
+}else {
+    body.style["backgroundColor"] = "white";
+    body.style["color"] = "black";
+}
